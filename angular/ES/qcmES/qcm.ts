bbb0d7bbe8698579570e91646e48dc0b37c18153
AlBob <>
2023-10-09 09:18:06 +0200

    up qcm

--- angular/ES/qcmES/qcm.ts
+++ angular/ES/qcmES/qcm.ts
@@ -3,9 +3,10 @@ import { QCM } from "src/app/data/QCM";
 export const qcmAboDesabo: QCM = {
     "code": "typescript",
     "answers": [
-        { "correct": false, "label": "constructor(private a:  number, private b: number, private f: (a: number, b: number) => number) {\\n  const c = f(a, b);\n}" }, 
-        { "correct": true, "label": "" }, 
-        { "correct": false, "label": "" }, 
-        { "correct": true, "label": "" }
+        { "correct": false, "label": "constructor(private L:  readonly number[], private f: (a: number) => Person) {\n  const L2 = L.map(f)\n}" }, 
+        { "correct": false, "label": "@Input() L2: readonly Person[];\nconstructor(private L:  readonly number[], private f: (a: number) => Person) {\n  this.L2 = L.map(f);\n}" }, 
+        { "correct": false, "label": "@Input({required: true}) L: readonly number[];\n@Input({required: true}) f: (a: number) => Person;\nreadonly L2: readonly Person[];\nconstructor() {\n  this.L2 = this.L.map(this.f);\n}" }, 
+        { "correct": true , "label": "interface STATE {\n  readonly L: readonly number[];\n  readonly f: (a: number) => Person;\n  readonly L2: readonly Person[];\n}\n@Component({...})\nclass C {\n  private _L = signal<readonly number[]>([])\n  @Input({required: true}) \n    get L( ) { return this._L() }\n    set L(v) { this._L.set(v)   }\n  private _f = signal<(a: number) => Person>( () => person )\n  @Input({required: true})\n    get f( ) { return this._f }\n    set f(v) { this._f.set(v) }\n  readonly state = computed<STATE>( () => ({\n    L: this._L(),\n    f: this._f(),\n    L2: this._L().map( this._f() )\n  }) );\n}" }, 
+        { "correct": false, "label": "private _L = signal<readonly number[]>([])\n@Input({required: true}) \n  get L( ) { return this._L() }\n  set L(v) { this._L.set(v)   }\nprivate _f = signal<(a: number) => Person>( () => person )\n@Input({required: true})\n  get f( ) { return this._f }\n  set f(v) { this._f.set(v) }\nreadonly L2 = computed<readonly Person[]>( () => this._L().map(this._f) );" }
     ] 
 }
--- angular/ES/qcmES/sujet.md
+++ angular/ES/qcmES/sujet.md
@@ -1,5 +1,5 @@
 # Exercice : E/S des composants Angular
 
-Vous devez développer un composant qui prend en entrée un nombre **`a`** (obligatoire), un nombre **`b`** (obligatoire) et qui aura besoin pour son fonctionnement interne d'un bombre **`c`** dérivé de **`a`** et **`b`** par la fonction **`f(a, b) => number`**.
+Vous devez développer un composant C qui prend en entrée un tableau de nombres en lecture seul **`L`** (obligatoire), une fonction **`f: (a: number) => Person`** (obligatoire). On suppose que pour le bon fonctionnement du composant vous avez besoin de calculer le map de L par f et de le stocker dans **`L2`** en tant que **`readonly Person[]`**. On suppose que vous disposez d'une personne par défaut `person: Person`.
 
-## Cochez les cases qui définissent un tel composant
+## Cochez les cases qui permettent de définir un tel composant en suivant les bonnes pratiques
