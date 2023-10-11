ff6260f2d03bcce132f74dec76b0c52724ca6e5e
AlBob <>
2023-10-11 11:43:14 +0200

    ++

--- processus/callbacks/sbChatsCB/exo.ts
+++ processus/callbacks/sbChatsCB/exo.ts
@@ -1,8 +1,8 @@
 import { EXO } from "src/app/data/EXO"
 
 export const exo_map: EXO = {
-    "title": "Exercice ; filter sur un tableau",
+    "title": "Exercice : Callbacks de chats",
     "type": "stackBlitz",
-    "statementURL": "processus/promise/sbChats/sujet.md",
+    "statementURL": "processus/promise/sbChatsCB/sujet.md",
     "stackBlitzURL": "https://stackblitz.com/edit/ptf-ens-l3m-processus-async-chats"
 }
--- processus/promise/sbChats/exo.ts
+++ processus/promise/sbChats/exo.ts
@@ -1,8 +1,8 @@
 import { EXO } from "src/app/data/EXO"
 
 export const exo_map: EXO = {
-    "title": "Exercice ; filter sur un tableau",
+    "title": "Exercice : Promesses de chats",
     "type": "stackBlitz",
-    "statementURL": "processus/callbacks/sbChatsCB/sujet.md",
+    "statementURL": "processus/promise/sbChats/sujet.md",
     "stackBlitzURL": "https://stackblitz.com/edit/ptf-ens-l3m-processus-async-chats"
 }
